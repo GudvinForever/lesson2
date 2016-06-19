20b340077b65c6ebad3d7cb4d00d111ca0aaa07d
oddsGold <ds.intelserv@gmail.com>
2016-06-19 14:52:40 +0300

    init

---
+++ js/firstjs.js
@@ -0,0 +1,1 @@
+<script type="text/javascript">function calendarOffnote() {var Dt=new Date(),day=Dt.getDate(),month=Dt.getMonth()+1,year=Dt.getFullYear(),tz=day+'-'+month+'-'+year;window.document.getElementById("calendarOffnote").src="http://www.offnote.net/calendar/informer/?bg=a0&tz="+tz;}calendarOffnote ();</script>
